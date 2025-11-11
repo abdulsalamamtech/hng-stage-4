f207400be39737bc91f686dbcc9cdf6c0c56bb91
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-11 13:47:24 +0100

    feat (api): modified link to v1 api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+
+// API Version 1 Routes
+Route::prefix('v1')->group(function () {
+    require __DIR__ . '/v1/api.php';
+});
