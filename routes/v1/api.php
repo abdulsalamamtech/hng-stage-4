881e42d86a727e973591b329338680ce0acbdcce
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-11 22:17:21 +0100

    feat (route): add more route endpoint

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -1,20 +1,13 @@
 <?php
 
-
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-// user login route
-// Route::post('/login', [AuthController::class, 'login']);
-// user registration route
-// Route::post('/register', [AuthController::class, 'register']);
 // protected route
 Route::middleware('auth:sanctum')->group(function () {
-    // Route::get('/profile', [AuthController::class, 'profile']);
-    // Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::get('/test', function (Request $request) {
-        return $request->all();
-    });
+    // User resource routes
+    Route::apiResource('users', UserController::class)
+        ->only(['index', 'show', 'update']);
 });
