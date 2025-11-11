8d6568d172208e46658e1230751ea9e438ab26c1
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-11 22:14:36 +0100

    feat (db): add push_token field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            // push_token optional
+            $table->string('push_token')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
