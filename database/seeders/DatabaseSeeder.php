19af8da1af6f64946399cf40edf5354e497b7ae0
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-12 17:34:53 +0100

    feat (db): add factory for db user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'password' => bcrypt('password'),
+        //     'push_token' => 'your_push_token_here',
+        // ]);
     }
 }
